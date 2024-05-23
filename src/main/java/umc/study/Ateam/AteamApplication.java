b9238066ddc1633904d5079a150b26bd7727117e
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-05-24 08:28:29 +0900

    chore : spring init

---
+++ src/main/java/umc/study/Ateam/AteamApplication.java
@@ -0,0 +1,13 @@
+package umc.study.Ateam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteamApplication.class, args);
+	}
+
+}
