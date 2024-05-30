7fecf72d157de06c86778428b9657e65b602c013
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-05-31 08:54:50 +0900

    feat : 도메인 정의

---
+++ src/main/java/umc/study/Ateam/domain/Terms.java
@@ -0,0 +1,30 @@
+package umc.study.Ateam.domain;
+
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.*;
+import umc.study.Ateam.domain.common.BaseEntity;
+import umc.study.Ateam.domain.mapping.MemberAgree;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Terms extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String body;
+
+    private Boolean optional;
+
+    @OneToMany(mappedBy = "terms", cascade = CascadeType.ALL)
+    private List<MemberAgree> memberAgreeList = new ArrayList<>();
+}
