88f2e2d239340cbf2b935f31a115cbd86ae83287
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-06-20 10:00:38 +0900

    feat : 내가 진행중인 미션 목록 조회

--- src/main/java/umc/study/Ateam/repository/MissionRepository.java
+++ src/main/java/umc/study/Ateam/repository/MissionRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import umc.study.Ateam.domain.Mission;
 import umc.study.Ateam.domain.Review;
+import umc.study.Ateam.domain.mapping.MemberMission;
 
 @Repository
 public interface MissionRepository extends JpaRepository<Mission, Long> {
--- src/main/java/umc/study/Ateam/web/controller/MissionController.java
+++ src/main/java/umc/study/Ateam/web/controller/MissionController.java
@@ -20,6 +20,7 @@ import umc.study.Ateam.web.dto.ReviewRequestDTO;
 public class MissionController {
 
     private final StoreQueryService storeQueryService;
+    private final MissionService missionService;
 
     @GetMapping("{storeId}/missions")
     @Operation(summary = "특정 가게의 미션 목록 조회",description = "사용자가 선택한 가게의 미션 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
@@ -32,9 +33,26 @@ public class MissionController {
     @Parameters({
             @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!")
     })
-    public ApiResponse<MissionResponseDTO.MissionPreViewListDTO> getMemberReviewList(@PathVariable(name = "storeId") Long storeId, @RequestParam(name = "page") Integer page){
+    public ApiResponse<MissionResponseDTO.MissionPreViewListDTO> getStoreMissionList(@PathVariable(name = "storeId") Long storeId, @RequestParam(name = "page") Integer page){
 
         MissionResponseDTO.MissionPreViewListDTO missions = (MissionResponseDTO.MissionPreViewListDTO) storeQueryService.getMissionList(storeId, page);
+        return ApiResponse.onSuccess(missions);
+    }
+
+    @GetMapping("{memberId}/missions")
+    @Operation(summary = "내가 진행 중인 미션 목록 조회",description = "내가 진행 중인 미션 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "memberId", description = "멤버의 아이디, path variable 입니다!")
+    })
+    public ApiResponse<MissionResponseDTO.MissionPreViewListDTO> getMemberMissionList(@PathVariable(name = "memberId") Long memberId, @RequestParam(name = "page") Integer page){
+
+        MissionResponseDTO.MissionPreViewListDTO missions = (MissionResponseDTO.MissionPreViewListDTO) missionService.getMemberMissionList(memberId, page);
         return ApiResponse.onSuccess(missions);
     }
 }
